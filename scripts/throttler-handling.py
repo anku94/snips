1fe7eee86bf30c31ce500a793e1318295d8bff7d
Ankush Jain <anku.94@gmail.com>
2024-08-15 16:19:37 -0400

    Add throttler handling

--- scripts/throttler-handling.py
+++ scripts/throttler-handling.py
@@ -3,10 +3,8 @@ import logging
 import paramiko
 import re
 import sys
-import time
 import os
 
-from multiprocessing import Pool
 from typing import Callable, TypedDict, Literal
 
 global logger
@@ -28,20 +26,6 @@ class Check(TypedDict):
 class CheckResult(TypedDict):
     goodnodes: list[str]
     badnodes: list[tuple[str, str]]
-
-
-def tmp():
-    experiments = ["amr22wfok02"]
-    hosts = get_initial_hosts(experiments)
-    hosts
-    sshm = SSHManager(hosts)
-    sort_by_hostname = lambda x: int(x[0].split(".")[0].replace("h", ""))
-    ret = sshm.run_cmd("hostname")
-    sorted(ret, key=sort_by_hostname)
-    p
-    cmd = "hostname -f | cut -d. -f 1"
-    ret = sshm.run_cmd(cmd)
-    sorted(ret, key=sort_by_hostname)
 
 
 class CustomFormatter(logging.Formatter):
@@ -87,23 +71,6 @@ def run_ssh_cmd(hostname: str, cmd: str) -> str:
     return output
 
 
-def get_node_name(hostname: str) -> str:
-    cmd = "hostname -f | cut -d. -f 1"
-    try:
-        output = run_ssh_cmd(hostname, cmd)
-        return output.strip()
-    except Exception as e:
-        logger.warning(f"-ERROR- Failed to get node name for host: {hostname}:\n {e}")
-        return "[UNKNOWN]"
-
-
-def get_node_names(hostnames: list[str]) -> list[str]:
-    nprocs = 16
-    with Pool(processes=nprocs) as pool:
-        results = pool.map(get_node_name, hostnames)
-        return list(results)
-
-
 def get_all_hosts(experiment: str) -> list[str]:
     root_host = f"h0.{experiment}.tablefs"
     cmd = "/share/testbed/bin/emulab-listall"
@@ -122,74 +89,6 @@ def get_our_exp_name() -> str:
     logger.debug(f"Experiment name: {exp_name}")
 
     return exp_name
-
-
-def check_throttling(hostname: str) -> bool:
-    cmd = "sudo dmesg | grep throttled"
-
-    try:
-        output = run_ssh_cmd(hostname, cmd)
-        is_throttling = bool(output.strip())
-    except Exception as e:
-        logger.warning(f"-ERROR- Failed to check host: {hostname}, assuming bad:\n {e}")
-        is_throttling = True
-
-    if is_throttling:
-        logger.warning(f"Throttling found on host: {hostname}")
-
-    return is_throttling
-
-
-def check_other_hw_issues(hostname: str) -> bool:
-    cmd = "sudo dmesg | egrep 'Hardware Error'"
-    is_bad = False
-
-    try:
-        output = run_ssh_cmd(hostname, cmd)
-        is_bad = bool(output.strip())
-    except Exception as e:
-        logger.warning(f"-ERROR- Failed to check host: {hostname}, assuming bad:\n {e}")
-        is_bad = True
-
-    if is_bad:
-        logger.warning(f"Generic hardware issues found on host: {hostname}")
-
-    return is_bad
-
-
-def check_memory_issues(hostname: str) -> bool:
-    cmd = "sudo dmesg | egrep 'MEMORY ERROR'"
-    is_bad = False
-
-    try:
-        output = run_ssh_cmd(hostname, cmd)
-        is_bad = bool(output.strip())
-    except Exception as e:
-        logger.warning(f"-ERROR- Failed to check host: {hostname}, assuming bad:\n {e}")
-        is_bad = True
-
-    if is_bad:
-        logger.warning(f"Memory issues found on host: {hostname}")
-
-    return is_bad
-
-
-def get_ip(hostname: str) -> str:
-    time.sleep(0.01)
-
-    cmd = "ifconfig | grep 10.94"
-    try:
-        output = run_ssh_cmd(hostname, cmd)
-        mobj = re.match(r"^.*inet ([0-9\.]+) .*$", output, re.DOTALL)
-        if mobj is None:
-            logger.warning(f"-ERROR- Failed to get IP for host: {hostname}")
-            logger.warning(f"-ERROR- Output: {output}")
-            return "ERROR"
-        else:
-            return mobj.group(1)
-    except Exception as e:
-        logger.warning(f"-ERROR- Failed to get IP for host: {hostname}:\n {e}")
-        return "ERROR"
 
 
 def get_ip_check() -> Check:
@@ -262,26 +161,6 @@ def execute_all_checks(
     return {"goodnodes": goodnodes, "badnodes": badnodes}
 
 
-def check_all_for_func(
-    hostnames: list[str], badness_func: Callable[[str], bool]
-) -> list[str]:
-    num_processes = min(len(hostnames), 32)
-
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(badness_func, hostnames)
-    bad_hosts = [hostname for hostname, is_bad in zip(hostnames, results) if is_bad]
-    return bad_hosts
-
-
-def get_all_ips(hosts: list[str]) -> list[str]:
-    num_processes = min(len(hosts), 32)
-
-    with Pool(processes=num_processes) as pool:
-        results = pool.map(get_ip, hosts)
-
-    return results
-
-
 def get_blacklist(blacklist_file: str) -> list[str]:
     if not os.path.exists(blacklist_file):
         return []
@@ -305,25 +184,7 @@ def write_to_blacklist(blacklist_file: str, hosts: list[str]):
     raise Exception("Not to be used!")
 
 
-def append_to_blacklist(blacklist_file: str, hosts: list[str], reason: str):
-    bad_nodes = get_node_names(hosts)
-
-    for h, n in zip(hosts, bad_nodes):
-        logger.warning(f"Blacklisting host: {h}/{n} (reason: {reason})")
-
-    with open(blacklist_file, "a") as f:
-        for h in hosts:
-            f.write(f"{h}\n")
-
-    key_func = lambda x: int(x.replace("wf", ""))
-    bad_nodes = ",".join(sorted(set(bad_nodes), key=key_func))
-
-    logger.warning(f"Blacklisted nodes: {bad_nodes}")
-
-
-def append_to_blacklist_new(
-    blacklist_file: str, blacklist_tuples: list[tuple[str, str]]
-):
+def append_to_blacklist(blacklist_file: str, blacklist_tuples: list[tuple[str, str]]):
     with open(blacklist_file, "a") as f:
         for h, _ in blacklist_tuples:
             logger.info(f"Blacklisting host: {h}")
@@ -356,60 +217,8 @@ def write_file(file_path: str, lines: list[str]):
         f.write("\n".join(lines))
 
 
-def get_valid_hosts(
-    experiments: list[str], work_dir: str
-) -> tuple[list[str], list[str]]:
-    hostsbyname_file = os.path.join(work_dir, "hostsbyname.txt")
-    hostsbyip_file = os.path.join(work_dir, "hostsbyip.txt")
-    hostsblacklisted_file = os.path.join(work_dir, "hostsblacklisted.txt")
-
-    if not os.path.exists(hostsbyname_file):
-        hosts = get_initial_hosts(experiments)
-    else:
-        hosts = read_file(hostsbyname_file)
-
-    blacklist_cur = get_blacklist(hostsblacklisted_file)
-    valid_hosts = [h for h in hosts if h not in blacklist_cur]
-
-    # throttling_hosts = check_all_for_badness(valid_hosts)
-    throttling_hosts = check_all_for_func(valid_hosts, check_throttling)
-    append_to_blacklist(hostsblacklisted_file, throttling_hosts, "throttling")
-
-    badmem_hosts = check_all_for_func(valid_hosts, check_memory_issues)
-    append_to_blacklist(hostsblacklisted_file, badmem_hosts, "badmem")
-
-    other_hw_issues = check_all_for_func(valid_hosts, check_other_hw_issues)
-    logger.warning("Not blacklisting nodes for other hw issues!")
-
-    valid_hosts = [h for h in valid_hosts if h not in throttling_hosts]
-    valid_host_ips = get_all_ips(valid_hosts)
-
-    hosts_and_ips = zip(valid_hosts, valid_host_ips)
-    hosts_ip_blacklist = []
-    for h, ip in hosts_and_ips:
-        if ip == "ERROR":
-            hosts_ip_blacklist.append(h)
-
-    valid_hosts = [h for h in valid_hosts if h not in hosts_ip_blacklist]
-    valid_host_ips = [ip for ip in valid_host_ips if ip != "ERROR"]
-
-    if len(hosts_ip_blacklist) > 0:
-        append_to_blacklist(hostsblacklisted_file, hosts_ip_blacklist, "ip-error")
-
-    write_file(hostsbyname_file, valid_hosts)
-    write_file(hostsbyip_file, valid_host_ips)
-
-    assert len(valid_hosts) == len(valid_host_ips)
-
-    logger.info(f"Found {len(valid_hosts)} valid hosts")
-
-    return valid_hosts, valid_host_ips
-
-
-def run_all_checks_main(hosts: list[str]) -> CheckResult:
+def run_all_checks_inner(hosts: list[str]) -> CheckResult:
     global logger
-    experiments: list[str] = [get_our_exp_name()]
-    hosts = get_initial_hosts(experiments)
 
     all_checks: list[Check] = [
         get_ip_check(),
@@ -421,29 +230,37 @@ def run_all_checks_main(hosts: list[str]) -> CheckResult:
     logger.info(f"Running {len(all_checks)} checks on {len(hosts)} hosts")
 
     sshm = SSHManager(hosts)
-    unreachable_nodes = sshm.get_unreachable_hosts()
-    unreachable_nodes
-    check_results = execute_all_checks(sshm, hosts, all_checks)
+    goodnodes: list[str] = []
+    badnode_list: list[tuple[str, str]] = []
 
-    goodnodes = check_results["goodnodes"]
-    goodnodes
-    badnode_list = check_results["badnodes"]
-    badnodes = [h for h, _ in badnode_list]
-    get_node_name_cmd = "hostname -f | cut -d. -f 1"
+    try:
+        unreachable_nodes = sshm.get_unreachable_hosts()
+        check_results = execute_all_checks(sshm, hosts, all_checks)
 
-    name_cmd_ret = sshm.run_cmd_on_hosts(badnodes, get_node_name_cmd)
-    badnodes, badnode_names = zip(*name_cmd_ret)
+        goodnodes = check_results["goodnodes"]
+        badnode_list = check_results["badnodes"]
+        badnodes = [h for h, _ in badnode_list]
 
-    for h, name in zip(badnodes, badnode_names):
-        logger.warning(f"Bad node: {h} ({name})")
+        get_node_name_cmd = "hostname -f | cut -d. -f 1"
+        name_cmd_ret = sshm.run_cmd_on_hosts(badnodes, get_node_name_cmd)
+        if len(name_cmd_ret):
+            badnodes, badnode_names = zip(*name_cmd_ret)
+        else:
+            badnodes = []
+            badnode_names = []
 
-    logger.warning(f"Comma-separated: {','.join(badnode_names)} (excl unreachable)")
+        for h, name in zip(badnodes, badnode_names):
+            logger.warning(f"Bad node: {h} ({name})")
 
-    badnode_list
+        logger.warning(f"Comma-separated: {','.join(badnode_names)} (excl unreachable)")
 
-    badnode_list += [(h, "unreachable") for h in unreachable_nodes]
-    logger.info(f"Good nodes: {len(goodnodes)}")
-    logger.info(f"Bad nodes: {len(badnodes)} (incl unreachable nodes)")
+        badnode_list += [(h, "unreachable") for h in unreachable_nodes]
+        logger.info(f"Good nodes: {len(goodnodes)}")
+        logger.info(f"Bad nodes: {len(badnodes)} (incl unreachable nodes)")
+    except Exception as e:
+        logger.error(f"Failed to run checks: {e}")
+    finally:
+        del sshm
 
     check_result: CheckResult = {
         "goodnodes": goodnodes,
@@ -453,7 +270,7 @@ def run_all_checks_main(hosts: list[str]) -> CheckResult:
     return check_result
 
 
-def main(work_dir: str) -> list[str]:
+def run_all_checks(work_dir: str) -> list[str]:
     experiments: list[str] = [get_our_exp_name()]
 
     hostsbyname_file = os.path.join(work_dir, "hostsbyname.txt")
@@ -465,7 +282,7 @@ def main(work_dir: str) -> list[str]:
     else:
         hosts = read_file(hostsbyname_file)
 
-    check_results = run_all_checks_main(hosts)
+    check_results = run_all_checks_inner(hosts)
     valid_hosts = check_results["goodnodes"]
     blacklist_tuples = check_results["badnodes"]
     blacklist = [h for h, _ in blacklist_tuples]
@@ -473,7 +290,8 @@ def main(work_dir: str) -> list[str]:
     logger.info(f"Blacklisting {len(blacklist)} nodes")
 
     write_file(hostsbyname_file, valid_hosts)
-    append_to_blacklist_new(hostsblacklisted_file, blacklist_tuples)
+    if len(blacklist_tuples) > 0:
+        append_to_blacklist(hostsblacklisted_file, blacklist_tuples)
 
     return valid_hosts
 
@@ -559,8 +377,7 @@ def run(output_file: str, randomize: bool = False):
 
     os.makedirs(work_dir, exist_ok=True)
 
-    # valid_hosts, valid_ips = get_valid_hosts(experiments, work_dir)
-    valid_hosts = main(work_dir)
+    valid_hosts = run_all_checks(work_dir)
 
     if randomize:
         import random
@@ -574,6 +391,5 @@ def run(output_file: str, randomize: bool = False):
 
 if __name__ == "__main__":
     setup_logging()
-    # do_things()
     args = parse_args()
     run(args.output_file, args.randomize)
